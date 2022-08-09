5b21f6ed5bf4364dd877cd49da90a87c5e217672
Ahsan Habib <ahsan34.07@gmail.com>
2022-08-09 17:34:28 +0600

    department data type

---
+++ ClientManagementService/Migrations/20220809113348_Department datatype.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClientManagementService.Migrations
+{
+    public partial class Departmentdatatype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
